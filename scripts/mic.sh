1f25042c70645ac640166e643073d3c0df664e1e
ViliamKovac1223 <viliamkovac1223@gmail.com>
2023-08-04 01:21:50 +0200

    mic script fix

--- scripts/mic.sh
+++ scripts/mic.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 # icon for microphone   
 
-volume=$(pactl list sources | grep "Source #[0-9]" -A 9 | tail -n1 | awk '{print $5}')
-muted=$( pactl list sources | grep "Source #[0-9]" -A 8 | tail -n1 | awk '{print $2}')
+volume=$(amixer get Capture | tail -n1 | awk '{print $5}' | tr -d [])
+muted=$(amixer get Capture | tail -n1 | awk '{print $6}' | tr -d [])
+if [ "$muted" = "on" ]; then
+    muted="no"
+fi
+
+# volume=$(pactl list sources | grep "Source #[0-9]" -A 9 | tail -n1 | awk '{print $5}')
+# muted=$( pactl list sources | grep "Source #[0-9]" -A 8 | tail -n1 | awk '{print $2}')
 
 if [ "$muted" = "no" ]; then
     echo "$volume";
