966c4cb14c5cf3a2ea6b7b8f026ce162a101025f
ViliamKovac1223 <viliamkovac1223@gmail.com>
2024-03-06 18:22:04 +0100

    Make system recognize default sound card

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -2,8 +2,9 @@
 # icon volume on:      \uea26
 # icon volume off:     \uea2a
 
-volume=$(pactl list sinks | grep -e '^[[:space:]]Volume:' | awk '{print $5}')
-muted=$(pactl list sinks | grep '^[[:space:]]Mute:' | head -n 1 | awk '{print $2}')
+defaultCard=$(pactl list  | grep -B 2 "Name: $(pactl get-default-sink)$" | head -n1  | awk '{print $2}' | tr -d '#')
+volume=$(pactl list sinks | grep -A 10 "Sink #$defaultCard" | grep -e '^[[:space:]]Volume:' | awk '{print $5}')
+muted=$(pactl list sinks  | grep -A 10 "Sink #$defaultCard" | grep '^[[:space:]]Mute:' | head -n 1 | awk '{print $2}')
 
 if [ "$muted" = "no" ]; then
     echo "  $volume";
---
+++ scripts/volumeMute.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+defaultCard=$(pactl list | grep -B 2 "Name: $(pactl get-default-sink)$" | head -n1  | awk '{print $2}' | tr -d '#')
+pactl set-sink-mute "$defaultCard" toggle
